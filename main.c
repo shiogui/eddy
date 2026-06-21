79d21929574da9289affaf283a4c67d6c9aae512
Shiogui Nagasse <shiogui@gmail.com>
2026-06-21 11:01:08 -0300

    added some basic forms

--- main.c
+++ main.c
@@ -4,9 +4,13 @@
 #include <SDL3/SDL.h>
 #include <SDL3/SDL_main.h>
 
+#define APP_WIDTH 800
+#define APP_HEIGHT 600
+
 typedef struct {
     SDL_Window *window;
     SDL_Renderer *renderer;
+    SDL_FPoint points[500];
     bool is_running;
 } State;
 
@@ -28,11 +32,19 @@ SDL_AppResult SDL_AppInit(void **appstate, int argc, char **argv)
 
     *appstate = state;
 
-    if (!SDL_CreateWindowAndRenderer("SDL3 Window Example", 800, 600, 0, &state->window, &state->renderer))
+    if (!SDL_CreateWindowAndRenderer("SDL3 Window Example", APP_WIDTH, APP_HEIGHT, 0, &state->window, &state->renderer))
     {
         return SDL_APP_FAILURE;
     }
 
+    SDL_SetRenderLogicalPresentation(state->renderer, APP_WIDTH, APP_HEIGHT, SDL_LOGICAL_PRESENTATION_LETTERBOX);
+
+    for (size_t i = 0; i < SDL_arraysize(state->points); i++)
+    {
+        state->points[i].x = (SDL_randf() * 440.0f) + 100.0f;
+        state->points[i].y = (SDL_randf() * 280.0f) + 100.0f;
+    }
+    
     state->is_running = true;
     
     return SDL_APP_CONTINUE;
@@ -43,8 +55,35 @@ SDL_AppResult SDL_AppIterate(void *appstate)
     SDL_Delay(16); // ~60 FPS
     State *state = (State *)appstate;
 
-    SDL_SetRenderDrawColor(state->renderer, 0, 0, 0, 255);
+    SDL_FRect rect;
+
+    SDL_SetRenderDrawColor(state->renderer, 33, 33, 33, SDL_ALPHA_OPAQUE);
     SDL_RenderClear(state->renderer);
+
+    // draw a filled rectangle in the middle of the canvas
+    SDL_SetRenderDrawColor(state->renderer, 0, 0, 255, SDL_ALPHA_OPAQUE);
+    rect.x = rect.y = 100;
+    rect.w = 440;
+    rect.h = 280;
+    SDL_RenderFillRect(state->renderer, &rect);
+
+    // draw some points across the canvas
+    SDL_SetRenderDrawColor(state->renderer, 255, 0, 0, SDL_ALPHA_OPAQUE);
+    SDL_RenderPoints(state->renderer, state->points, SDL_arraysize(state->points));
+    
+    // draw a unfilled rectangle in-set a little bit
+    SDL_SetRenderDrawColor(state->renderer, 0, 255, 0, SDL_ALPHA_OPAQUE);
+    rect.x = 30;
+    rect.y = 30;
+    rect.w = 60;
+    rect.h = 60;
+    SDL_RenderFillRect(state->renderer, &rect);
+
+    // draw two lines in an X across the whole canvas
+    SDL_SetRenderDrawColor(state->renderer, 255, 255, 0, SDL_ALPHA_OPAQUE);
+    SDL_RenderLine(state->renderer, 0, 0, 640, 480);
+    SDL_RenderLine(state->renderer, 0, 480, 640, 0);
+    
     SDL_RenderPresent(state->renderer);
 
     return SDL_APP_CONTINUE;
