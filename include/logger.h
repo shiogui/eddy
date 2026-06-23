1044a4e455bbd0b6b0b985a1c75b6f8e73ea0c1b
Shiogui Nagasse <shiogui@gmail.com>
2026-06-23 10:26:53 -0300

    improved logging: - added log_setup which logs startup steps - added log_to_file which logs to the specified file - added log_to_file_with_timestamp which logs to the specified file adding a timestamp

--- include/logger.h
+++ include/logger.h
@@ -2,5 +2,13 @@
 #define LOGGER_H
 
 void log_info(char *format, ...);
+void log_setup(char *step, char *status);
+
+/// @brief Logs the content to a desired file
+/// @param file Pointer to an open and write enabled file
+/// @param format Message/format to be written out
+/// @param args Arguments to be interpolated into the format
+void log_to_file(FILE *file, char *format, ...);
+void log_to_file_with_timestamp(FILE *file, char *format, ...);
 
 #endif
--- src/utils/logger.c
+++ src/utils/logger.c
@@ -1,6 +1,6 @@
-
 #include <stdio.h>
 #include <stdarg.h>
+#include <time.h>
 
 #include "app.h"
 #include "logger.h"
@@ -13,4 +13,37 @@ void log_info(char *format, ...)
     vprintf(format, args);
     va_end(args);
     printf("\n");
+}
+
+void log_setup(char *step, char *status)
+{
+    printf("[%s] [SETUP] %-10s - %s\n", APP_NAME, step, status);
+}
+
+void log_to_file(FILE *file, char *format, ...)
+{
+    fprintf(file, "[%s] ", APP_NAME);
+    va_list args;
+    va_start(args, format);
+    vfprintf(file, format, args);
+    va_end(args);
+    fprintf(file, "\n");
+}
+
+void log_to_file_with_timestamp(FILE *file, char *format, ...)
+{
+    time_t now;
+    struct tm *time_info;
+    char buffer[20];
+
+    time(&now);
+    time_info = localtime(&now);
+    strftime(buffer, sizeof(buffer), "%Y-%m-%d %H:%M:%S", time_info);
+
+    fprintf(file, "[%s] [%s] ", APP_NAME, buffer);
+    va_list args;
+    va_start(args, format);
+    vfprintf(file, format, args);
+    va_end(args);
+    fprintf(file, "\n");
 }
